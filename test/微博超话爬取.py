d856686278baeaa7a618532c33eaebd2b15e0b83
teapot-4l8 <chaiyunze@gmail.com>
2024-11-10 13:25:36 +0800

    update weibo

--- test/微博超话爬取.py
+++ test/微博超话爬取.py
@@ -26,34 +26,44 @@ headers = {
     'x-xsrf-token': '5fa3ae',
 }
 
-params = {
-    'extparam': '肖宇梁',
-    'containerid': '100808abb887d7734e4121eef9853b451c11b9',
-    # 'luicode': '20000061',
-    # 'lfid': '5095189509047708',
-}
 
-response = requests.get('https://m.weibo.cn/api/container/getIndex', params=params,headers=headers)
-# print(response.json())
-cards = response.json()['data']['cards']
-# print(cards)
+def send_get_request():
+    response = requests.get('https://m.weibo.cn/api/container/getIndex', params=params,headers=headers)
+    # print(response.json())
+    data = response.json()['data']
+    cards = data['cards']
+    # print(cards)
+    since_id = data['pageInfo']['since_id']
+    return cards, since_id
 
 def is_target(text):
     return bool(re.search(r'『五号星球』', text))  # 检查text中是否包含"『五号星球』"
 
+def parse_response(card):
+    mblog = card['mblog']
+    created_at = mblog['created_at']
+    text = mblog['text']
+    id_ = mblog['id']
+    target_page_url = DOMAIN + id_
+    if is_target(text):
+        print(created_at)
+        print(text)
+        print(id_)
+        print(target_page_url)
+        print("=============================================")
 
-for card in cards:
-    try:
-        mblog = card['mblog']
-        created_at = mblog['created_at']
-        text = mblog['text']
-        id_ = mblog['id']
-        target_page_url = DOMAIN + id_
-        if is_target(text):
-            print(created_at)
-            print(text)
-            print(id_)
-            print(target_page_url)
-            print("=============================================")
-    except KeyError:
-        pass
+
+if __name__ == '__main__':
+    params = {
+        'extparam': '肖宇梁',
+        'containerid': '100808abb887d7734e4121eef9853b451c11b9',
+        # 'luicode': '20000061',
+        # 'lfid': '5095189509047708',
+        # "since_id": "5099143848529594"  # 靠这里翻页
+    }
+    cards, since_id = send_get_request()
+    for card in cards:
+        try:
+            parse_response(card)
+        except KeyError:
+            pass
